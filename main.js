020b417733c69dbb0253aab22d4afafa1d7c37e2
akhilshyam74 <89392108+akhilshyam74@users.noreply.github.com>
2021-12-18 15:10:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ nosex = 0;
 nosey = 0;
 choice = null;
 function preload(){
-    clown_nose = loadImage("https://i.postimg.cc/4yRPBWG9/clownnose.png");
+
 }
 function setup()
 {
@@ -18,48 +18,16 @@ function setup()
     poseNet.on("pose", gotPoses);
 }
 function draw(){
-    if (choice = "ci"){
-        image(video, 0, 0, 300, 300);
-        console.log("Image = video, 0, 0, 300, 300");
-        fill(255, 0, 0);
-        console.log("Fill = 255, 0, 0")
-        stroke(255, 0, 0);
-        console.log("Stroke = 255, 0, 0")
-        circle(nosex, nosey, 20)
-        console.log("Circle = Nose X"+ nosex + "Nose Y"+ nosey + "20")
-        console.log("Drawing circle")
-    }
-    else if (choice = "cln"){
-        image(video, 0, 0, 300, 300);
-    console.log("Image = video, 0, 0, 300, 300");
-    image(clown_nose, nosex, nosey, 30, 30);
-    console.log("Drawing clown nose");
-    }
-    else{
-        console.log("Enter any one option to proceed")
-    }
+   
 }
 function take_snapshot()
 {
-    save('Clown filter image screenhot');
+   
 }
 function modelLoaded(){
-    console.log("PoseNet Is Initialized");
+  
 }
 function gotPoses(results)
 {
-    if(results.length > 0)
-    {
-        console.log(results);
-        nosex = results[0].pose.nose.x;
-        nosey = results[0].pose.nose.y;
-        console.log("nose x = " + nosex);
-        console.log("nose y = " + nosey);
-    }
-}
-function drawcircle(){
-    choice = "ci"
-}
-function drawclown_nose(){
-    choice = "cln"
+  
 }
